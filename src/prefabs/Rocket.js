c11bebb4a994299f3ffc82b77b9de4c84be14b66
Nick Marigo <nmarigo@ucsc.edu>
2026-01-17 23:07:19 -0800

    Saving

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -13,10 +13,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     update() {
         // left/right movement
-        if(this.isFiring) {
+        if(!this.isFiring) {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISIze - this.width) {
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }
@@ -37,9 +37,13 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = game.config.height - borderUISize - borderPadding;
         }
 
-
     }
 
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
 
 
 }
